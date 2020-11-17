2f55d7f480d93487e805c03700b92edfacf25775
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-11-17 20:50:24 +0100

    close chan reflect POC implementaion

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"C"
 	"fmt"
+	"reflect"
+	"sync"
 	"time"
 	"unsafe"
 
@@ -28,6 +30,22 @@ func chanrecv(c *hchan, ep unsafe.Pointer, block bool) (selected, received bool)
 
 //go:linkname memmove runtime.memmove
 func memmove(dst, src unsafe.Pointer, size int)
+
+var cvalues sync.Map
+
+func Close(ch interface{}, val interface{}) error {
+	chRef := reflect.ValueOf(ch)
+	chTyp := chRef.Type()
+	if chTyp.Kind() != reflect.Chan || chTyp.ChanDir()&reflect.SendDir == 0 {
+		return fmt.Errorf("provided entity type %q is not a writable channel", chTyp.Kind())
+	}
+	if valTp := reflect.ValueOf(val).Type(); valTp.Kind() != chTyp.Elem().Kind() {
+		return fmt.Errorf("provided value type %q doesn't match provided channel type %q", valTp.Kind(), chTyp.Elem().Kind())
+	}
+	cvalues.Store(ch, val)
+	chRef.Close()
+	return nil
+}
 
 func main() {
 	monkey.Patch(chanrecv1, func(c *hchan, elem unsafe.Pointer) {
