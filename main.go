a9ab9874e313d7679fe97169c83aec0cfc0ccbcd
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-11-17 21:37:49 +0100

    implement load from cached close ch value

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ type hchan struct {
 	closed uint32
 }
 
+type eface struct {
+	typ, val unsafe.Pointer
+}
+
 //go:linkname chanrecv1 runtime.chanrecv1
 func chanrecv1(c *hchan, elem unsafe.Pointer)
 
@@ -42,25 +46,29 @@ func Close(ch interface{}, val interface{}) error {
 	if valTp := reflect.ValueOf(val).Type(); valTp.Kind() != chTyp.Elem().Kind() {
 		return fmt.Errorf("provided value type %q doesn't match provided channel type %q", valTp.Kind(), chTyp.Elem().Kind())
 	}
-	cvalues.Store(ch, val)
+	cvalues.Store(chRef.Pointer(), val)
 	chRef.Close()
 	return nil
 }
 
+func cload(ch *hchan, elem unsafe.Pointer) {
+	if ch.closed == 1 {
+		ptr := uintptr(unsafe.Pointer(ch))
+		if val, ok := cvalues.Load(ptr); ok {
+			vptr := (*eface)(unsafe.Pointer(&val)).val
+			memmove(elem, vptr, int(unsafe.Sizeof(vptr)))
+		}
+	}
+}
+
 func main() {
-	monkey.Patch(chanrecv1, func(c *hchan, elem unsafe.Pointer) {
-		chanrecv(c, elem, true)
-		if c.closed == 1 {
-			val := 10
-			memmove(elem, unsafe.Pointer(&val), 8)
-		}
+	monkey.Patch(chanrecv1, func(ch *hchan, elem unsafe.Pointer) {
+		chanrecv(ch, elem, true)
+		cload(ch, elem)
 	})
-	monkey.Patch(chanrecv2, func(c *hchan, elem unsafe.Pointer) (received bool) {
-		_, received = chanrecv(c, elem, true)
-		if c.closed == 1 {
-			val := 10
-			memmove(elem, unsafe.Pointer(&val), 8)
-		}
+	monkey.Patch(chanrecv2, func(ch *hchan, elem unsafe.Pointer) (received bool) {
+		_, received = chanrecv(ch, elem, true)
+		cload(ch, elem)
 		return
 	})
 	ch := make(chan int, 2)
@@ -71,7 +79,7 @@ func main() {
 		}
 	}()
 	time.Sleep(time.Second)
-	close(ch)
+	Close(ch, 23)
 	v, ok := <-ch
 	fmt.Println("RESULT", v, ok)
 	v, ok = <-ch
